da2a3383c6a41936fe44bb1589d95deabaa51e50
Sean Livingston <sean.d.livingston@gmail.com>
2022-12-07 13:32:24 -0500

    Initial Commit

---
+++ SwiftUIMVVM/SwiftUIMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMVVMApp.swift
+//  SwiftUIMVVM
+//
+//  Created by Sean Livingston on 12/7/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
